85f3ca73a031747d84b51e7261502284e7b754c2
amatree <tri.tran.pmmp@gmail.com>
2022-10-22 12:44:57 -0700

    not finshed

--- 10. MergeSortedArray.py
+++ 10. MergeSortedArray.py
@@ -3,28 +3,50 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        if m == 0: 
-            nums1.pop(0)
-            nums1.extend(nums2)
-            return
         if n == 0:
             return
-        
+        if m == 0:
+            for i, v in enumerate(nums2):
+                nums1[i] = v
+            return
+            
+        j = 0
+        curr = nums2[j]
         for i in range(m + n):
-            if i < n:
-                print(nums2[i], " vs ", nums1[i])
-                if nums1[i] <= nums2[i]:
-                    if nums1[i+1] >= nums2[i]:
-                        nums1.insert(i+1, nums2[i])
-                        nums1.pop()
-            else:
+            if j == n:
                 break
-        
-        
+            if i < n:
+                # print(f"{curr} vs {nums1[i+1]}")
+                if nums1[i] <= curr:
+                    while i < m and nums1[i] < curr:
+                        # if nums1[i+1] == 0: break
+                        i += 1
+                    # if nums1[i] == 0: nums1.insert(i, curr)
+                    # else: nums1.insert(i+1, curr)
+                    nums1.insert(i, curr)
+                    # print(f"*inserted {curr} to nums1[{i}]") 
+                else:
+                    nums1.insert(i+1, curr)
+                    # print(f"**inserted {curr} to nums1[{i+1}]")
+                nums1.pop()
+                if j < n: 
+                    j += 1
+                    curr = nums2[j]
+            if i+1 >= m:
+                for i, v in enumerate(nums2, start=j):
+                    nums1[i+1] = v
+                return
+            if i >= n:
+                nums1[-1] = nums2[-1]
+                break
                 
 s = Solution()
-nums1 = [1,2,3,0,0,0]
-nums2 = [2,5,6]
-s.merge(nums1, 3, nums2, 3)
 
-print(nums1)
+nums1 = [1,2,3,0,0,0]
+nums2 = [4,5,6]
+
+print(f"Merging \n'nums1': {nums1}\n'nums2': {nums2}")
+
+s.merge(nums1, len(nums1) - nums1.count(0), nums2, len(nums2))
+
+print(f"--> {nums1}")
